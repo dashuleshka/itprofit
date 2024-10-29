1043661178a4815da32ef649980ea5686c5eed29
dashuleshka <lakmand3@gmail.com>
2024-10-29 18:05:04 +0300

    divided code in modal.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import IMask from "imask";
 import "../styles/style.scss";
 
+import { initializeModal } from "./modal";
+
 import {
   isValidEmail,
   validateField,
@@ -35,39 +37,4 @@ buttonForm.addEventListener("click", async (event) => {
   });
 });
 
-const modal = document.getElementById("modal");
-const openModalButton = document.getElementById("openModal");
-const yesButton = document.getElementById("yesButton");
-const noButton = document.getElementById("noButton");
-const overlay = document.createElement("div");
-
-overlay.classList.add("modal-overlay");
-document.body.appendChild(overlay);
-
-const disableScroll = () => {
-  const scrollBarWidth =
-    window.innerWidth - document.documentElement.clientWidth;
-  document.body.style.overflow = "hidden";
-  document.body.style.marginRight = `${scrollBarWidth}px`;
-};
-
-const enableScroll = () => {
-  document.body.style.overflow = "";
-  document.body.style.marginRight = "";
-};
-
-openModalButton.addEventListener("click", () => {
-  modal.classList.add("show");
-  overlay.classList.add("show");
-  disableScroll();
-});
-
-yesButton.addEventListener("click", closeModal);
-noButton.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-function closeModal() {
-  modal.classList.remove("show");
-  overlay.classList.remove("show");
-  enableScroll();
-}
+initializeModal();
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,23 +1,38 @@
-const modal = document.getElementById("modal");
-const openModalButton = document.getElementById("openModal");
-const closeButton = document.querySelector(".close-button");
-const overlay = document.createElement("div");
-
-overlay.classList.add("modal-overlay");
-
-document.body.appendChild(overlay);
-
-openModalButton.addEventListener("click", () => {
-  modal.classList.add("show");
-  overlay.classList.add("show");
-  document.body.style.overflow = "hidden";
-});
-
-closeButton.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-function closeModal() {
-  modal.classList.remove("show");
-  overlay.classList.remove("show");
-  document.body.style.overflow = "";
+export const initializeModal = () => {
+    const modal = document.getElementById("modal");
+    const openModalButton = document.getElementById("openModal");
+    const yesButton = document.getElementById("yesButton");
+    const noButton = document.getElementById("noButton");
+    const overlay = document.createElement("div");
+    
+    overlay.classList.add("modal-overlay");
+    document.body.appendChild(overlay);
+    
+    const disableScroll = () => {
+      const scrollBarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+      document.body.style.overflow = "hidden";
+      document.body.style.marginRight = `${scrollBarWidth}px`;
+    };
+    
+    const enableScroll = () => {
+      document.body.style.overflow = "";
+      document.body.style.marginRight = "";
+    };
+    
+    openModalButton.addEventListener("click", () => {
+      modal.classList.add("show");
+      overlay.classList.add("show");
+      disableScroll();
+    });
+    
+    yesButton.addEventListener("click", closeModal);
+    noButton.addEventListener("click", closeModal);
+    overlay.addEventListener("click", closeModal);
+    
+    function closeModal() {
+      modal.classList.remove("show");
+      overlay.classList.remove("show");
+      enableScroll();
+    }  
 }
